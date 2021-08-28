f786fa2369e326cdbea42fb6734617633b8cb2e9
Christine Coenen <code.coenen@gmail.com>
2021-08-28 14:06:51 +0200

    Move touch sensor debug output into own function

--- src/test-scripts/touch-test.py
+++ src/test-scripts/touch-test.py
@@ -7,8 +7,12 @@ from piripherals import MPR121
 
 mpr = MPR121(bus=4, irq=0)
 
+def _onTouch(isPressed, pinNumber):
+	pressedMessage = "pressed" if isPressed else "released"
+	print('pin %d is %s' % (pinNumber, pressedMessage))
+
 for i in range(12):
 	# print status on touch and release
-    mpr.on_touch(i, lambda *x: print(f'pin {x[1]} is {"pressed" if x[0] else "released"}'))
+    mpr.on_touch(i, _onTouch)
 
 input("Touch a pin to generate output. Press Enter stop...")
